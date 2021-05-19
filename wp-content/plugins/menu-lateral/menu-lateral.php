09a1ac39addc133dfb12b94037443d28d22cb645
Marco <marco.com17@gmail.com>
2021-05-19 12:14:56 -0500

    Preparacion para empezar a programar el menu lateral (plugin)

---
+++ wp-content/plugins/menu-lateral/menu-lateral.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Plugin Name: Menu lateral mejorado
+ * Plugin URI: https://marko.com.mx
+ * Description: Menu lateral especializado para la pagina del palacio
+ * Author: MarkoTF
+ * Version: 1.0
+ * */
+
+if (is_admin()) {
+  require_once __DIR__ . '/admin/admin.php';
+} else {
+  require_once __DIR__ . '/public/frontend.php';
+}
---
+++ wp-content/plugins/menu-lateral/public/frontend.php
@@ -0,0 +1,3 @@
+<?php
+wp_enqueue_script('mlm_frontend_js', WP_PLUGIN_DIR . '/menu');
+wp_enqueue_style()
